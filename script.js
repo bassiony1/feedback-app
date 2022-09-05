12c116575d30a4ab3a67ebcc2f3b2a69a361cbf5
Mahmoud Bassiony <beso.beso2468@gmail.com>
2022-09-05 14:15:32 +0200

    Added Editing Feature

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ Commentinput = document.querySelector('.comment-input');
 errormessage = document.querySelector('.error-msg');
 let rate = 10 ;
 let inputvalue = '';
+edit = false ;
 radiobtns = [...document.querySelectorAll('.radio-check')];
 commentBtn =document.querySelector('.comment-btn');
 tempcommment = document.querySelector('template');
@@ -9,22 +10,81 @@ review_num = document.querySelector('.review-num');
 comments = [...document.querySelectorAll('.comment-card')];
 avgrate = document.querySelector('.review-rate');
 deleteBtns = document.querySelectorAll('.comment-delete');
-
+editBtns = document.querySelectorAll('.comment-edit');
+let editingcomment = null;
 const getReviews = () => { review_num.innerText = `${comments.length} Reviews` ;}
 
+const deleteing = (e)=>{
+    let btn = e.target ;
+    document.querySelector('.main-comment').removeChild(btn.closest('.comment-card'));
+    comments = [...document.querySelectorAll('.comment-card')];
+    getReviews();
+    avg();
+}
 const avg = () => { 
     avgrate.innerText = `Average Rating:  ${comments.length==0 ? 0 :(comments.map((c) => {
-    return +c.querySelector('.comment-rate').innerText}).reduce((r , acc)=>{return acc += r;}, 0)/comments.length).toFixed(1)}`}
+    return +c.querySelector('.comment-rate').innerText}).reduce((r , acc)=>{return acc += r;}, 0)/comments.length).toFixed(1)}`
+}
+const commentAdd = (e)=>{
+    e.preventDefault();
 
+    let comment_card = document.createElement('div');
+    comment_card.classList.add('comment-card',  'bg-dark' ,'bg-opacity-75');
+    comment_card.innerHTML =
+        `<div class="comment-rate">${rate}</div>
+        <div class="comment-control flex-row ">
+            <i class="comment-edit fa fa-edit"></i>
+            <i class=" comment-delete fa fa-delete-left"></i>
+        </div>
+        <div class="comment-content">${inputvalue}</div>
+        `;
+
+    document.querySelector('.main-comment').prepend(comment_card);
+
+    inputvalue = '';
+    Commentinput.value = '';
+    comment_card.querySelector('.comment-delete').addEventListener('click', deleteing);
+    comment_card.querySelector('.comment-edit').addEventListener('click', editing);
+    comments = [...document.querySelectorAll('.comment-card')]
+    avg();
+    commentBtn.disabled = true;
+}
+const commentEdit = (e , comment)=>{
+    e.preventDefault();
+    editingcomment.querySelector('.comment-content').innerText = inputvalue;
+    editingcomment.querySelector('.comment-rate').innerText = rate;
+    getReviews();
+    avg();
+    edit = false ;
+    commentBtn.innerText = 'Add';
+    inputvalue = '';
+    Commentinput.value = '';
+    editingcomment = null;
+    commentBtn.disabled = true;
+}
+const editing = (e)=>{
+    let btn = e.target ;
+    editingcomment = btn.closest('.comment-card');
+    content = editingcomment.querySelector('.comment-content');
+    rate =  +editingcomment.querySelector('.comment-rate').innerText;
+    commentBtn.innerText = 'Edit';
+    commentBtn.disabled = false;
+    Commentinput.value = content.innerText;
+    inputvalue = content.innerText;
+    radiobtns[rate-1].checked = true ;
+    edit = true ;
+    Commentinput.focus();
+}
 avg();
+getReviews();
 Commentinput.addEventListener('input' , (e)=>{
     inlen = e.target.value.length;
     inputvalue = e.target.value;
-    if(inlen < 2  ){
+    if(inlen < 2  && edit === false){
         errormessage.classList.remove('d-none')
         errormessage.classList.add('d-block')
         commentBtn.disabled = true ;
-    }else if (inlen > 2 ){
+    }else if (inlen > 2 || edit=== true  ){
         errormessage.classList.remove('d-block')
         errormessage.classList.add('d-none')
         commentBtn.disabled = false ;
@@ -39,35 +99,21 @@ radiobtns.forEach((rb)=>{
     })
 })
 
-commentBtn.addEventListener('click' , (e)=>{
-    e.preventDefault();
-    let comment_card = document.createElement('div');
-    comment_card.classList.add('comment-card',  'bg-dark' ,'bg-opacity-75');
-    comment_card.innerHTML =
-        `<div class="comment-rate">${rate}</div>
-        <div class="comment-control flex-row ">
-            <i class="comment-edit fa fa-edit"></i>
-            <i class=" comment-delete fa fa-delete-left"></i>
-        </div>
-        <div class="comment-content">${inputvalue}</div>
-        `;
+commentBtn.addEventListener('click' , (e) => {
+    if (edit === false)    
+    commentAdd(e);
+    else if (edit === true && editingcomment !== null)
+    commentEdit(e , editingcomment);
 
-    document.querySelector('.main-comment').prepend(comment_card);
-    inputvalue = '';
-    Commentinput.value = '';
-    comments = [...document.querySelectorAll('.comment-card')]
-    getReviews();
-    avg();
-    commentBtn.disabled = true;
-})
+} );
+
 deleteBtns.forEach((dB)=>{
 
-    dB.addEventListener('click' , (e)=>{
-        btn = e.target ;
-        document.querySelector('.main-comment').removeChild(btn.closest('.comment-card'));
-        comments = [...document.querySelectorAll('.comment-card')];
-        getReviews();
-        avg();
-    })
+    dB.addEventListener('click' ,  deleteing);
 
+})
+
+
+editBtns.forEach((EB)=>{
+    EB.addEventListener('click',editing);
 })
